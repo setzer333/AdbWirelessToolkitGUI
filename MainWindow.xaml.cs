11793f70753d0f8a8c0fe6f7c202c9f280436248
setzer333 <sqnymqs@outlook.com>
2026-08-21 12:22:53 -0600

    este es un programa para poder hacer envios a android adb mas ffacilAgregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace AdbWirelessToolkitGUI;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    // Eventos de los botones ADB
+    private void MdnsScan_Click(object sender, RoutedEventArgs e) => Log("Escaneo mDNS iniciado");
+
+    private void PairDevice_Click(object sender, RoutedEventArgs e) => Log("Emparejando dispositivo");
+
+    private void ConnectIp_Click(object sender, RoutedEventArgs e) => Log("Conectando por IP");
+
+    private void InstallApk_Click(object sender, RoutedEventArgs e) => Log("Instalando APK");
+
+    private void TransferFile_Click(object sender, RoutedEventArgs e) => Log("Iniciando transferencia de archivo");
+
+    private void RestartBootloader_Click(object sender, RoutedEventArgs e) => Log("Reiniciando bootloader");
+
+    // Método para agregar logs a la consola
+    private void Log(string message)
+    {
+        Dispatcher.Invoke(() =>
+        {
+            LogConsole.AppendText($"{DateTime.Now:HH:mm:ss} | {message}\n");
+            // Auto-scroll al final
+            LogConsole.ScrollToEnd();
+        });
+    }
+}
